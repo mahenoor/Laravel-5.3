899fe284b5144934e394b4ad355a8d9499f29c51
vagrant <vagrant@homestead>
2017-01-30 09:52:49 +0000

    implemented some minor changes in StudentsController.php file

---
+++ laravel_crud/database/migrations/2017_01_25_120003_create_student_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('Gender');
+            $table->string('Department');
+            $table->string('Sports');
+            $table->string('Colors');
+            $table->integer('Physics');
+            $table->integer('Chemistry');
+            $table->integer('Maths');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students');
+    }
+}
